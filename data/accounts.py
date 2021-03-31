870b172b49470192a0c7120b6f8c9c2a776f5a18
Semen <megadeadpool228@mail.ru>
2021-03-31 20:49:00 +0300

    добавил регистрацию пользователя, подключил базу данных

---
+++ data/accounts.py
@@ -0,0 +1,20 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Accounts(SqlAlchemyBase):
+    __tablename__ = 'accounts'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
---
+++ data/users.py
@@ -0,0 +1,40 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.fields.simple import BooleanField
+from wtforms.validators import DataRequired
+from sqlalchemy import orm
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+    accounts = orm.relation("Accounts", back_populates='user')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
