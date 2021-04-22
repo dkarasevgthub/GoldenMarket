e40a09d269c1086c46762002f1b0621f1acb851a
Semen <megadeadpool228@mail.ru>
2021-04-22 21:44:28 +0300

    вернул обрез фотографии (он почему-то пропал) и вернул try-except при загрузке фото.

--- main.py
+++ main.py
@@ -651,6 +651,5 @@ def sorted_market(category):
 
 if __name__ == '__main__':
     db_session.global_init("db/logins.db")
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
